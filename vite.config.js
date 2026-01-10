de81066260a5494713fc5a6e39c7a96ee3b4b518
Bruno Souza <brunopw25@gmail.com>
2026-01-09 23:19:39 -0300

    correção do vite para lidar com o docker

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -8,11 +8,16 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        vue(),
     ],
     server: {
+        host: '0.0.0.0',  // Esta linha é crucial para Docker!
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            usePolling: true,
         },
     },
 });

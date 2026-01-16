ff2c5a701df6e0e790dae9e8809569e51ccfc399
Bruno Souza <brunopw25@gmail.com>
2026-01-15 22:18:32 -0300

    Reactive reference from reactive object

--- composition-api/src/App.vue
+++ composition-api/src/App.vue
@@ -1,11 +1,61 @@
-<script setup></script>
+<script>
+import { ref, reactive, toRef, toRefs } from "vue"
+export default {
+    setup() {
+        const message = ref("hello");
+        const quantity = ref(1);
+        const item = reactive({
+            name: "product 1",
+            price: 10
+        })
+
+        const increment = () => quantity.value++
+        const decrement = () => quantity.value--
+
+        const swapProduct = () => {
+            item.name = "product A"
+            item.price = 30
+        }
+
+        // const nameRef = toRef(item, "name");
+        //
+        // console.log("nameref:", nameRef.value)
+        //
+        // item.name = "new product"
+        //
+        // console.log("nameref:", nameRef.value)
+
+        const { name, price } = toRefs(item)
+
+        // const itemRefs = toRefs(item)
+        // console.log("name:", itemRefs.name.value)
+        // console.log("price:", itemRefs.price.value)
+        // itemRefs.name.value = "product 2"
+        // itemRefs.price.value = 40
+        // console.log("name:", itemRefs.name.value)
+        // console.log("price:", itemRefs.price.value)
+
+        return {
+            message,
+            quantity,
+            increment,
+            decrement,
+            name,
+            price,
+            swapProduct
+        }
+    }
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+    <h1>{{ name }} : {{ price }}</h1>
+    <h2>{{ quantity }}</h2>
+    <button @click="swapProduct">Swap</button>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+
+
 </template>
 
 <style scoped></style>

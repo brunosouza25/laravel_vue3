49341a00e762a52745d1d9d203178e67e8149ae5
Bruno Souza <brunopw25@gmail.com>
2026-01-18 10:32:02 -0300

    Props and emitting events

--- composition-api/src/App.vue
+++ composition-api/src/App.vue
@@ -1,77 +1,28 @@
 <script>
-import { computed, ref, reactive, toRef, toRefs, watch, watchEffect } from "vue"
+import {ref} from "vue"
+import CartItem from "@/components/CartItem.vue";
+
 export default {
-    setup() {
-        const item = reactive({
-            name: "product 1",
-            price: 10,
-            quantity: 1
-        })
-
-        const increment = () => item.quantity++
-        const decrement = () => item.quantity--
-
-        const swapProduct = () => {
-            item.name = "product A"
-            item.price = 30
-        }
-
-        // const nameRef = toRef(item, "name");
-        //
-        // console.log("nameref:", nameRef.value)
-        //
-        // item.name = "new product"
-        //
-        // console.log("nameref:", nameRef.value)
-
-        const { name, price, quantity } = toRefs(item)
-
-
-
-        // const itemRefs = toRefs(item)
-        // console.log("name:", itemRefs.name.value)
-        // console.log("price:", itemRefs.price.value)
-        // itemRefs.name.value = "product 2"
-        // itemRefs.price.value = 40
-        // console.log("name:", itemRefs.name.value)
-        // console.log("price:", itemRefs.price.value)
-
-        const total = computed(() => item.price * quantity.value)
-
-        watch(() => item.quantity, () => {
-            if (item.quantity === 5){
-                alert("you cannot add more item")
+    components: { CartItem },
+    setup(){
+        const items = ref([
+            {
+                id:1,
+                name: "Product X",
+                price: 20,
+                quantity: 1
             }
-        }, { immediate: true})
+        ])
 
-        watchEffect(() => {
-            console.log("price changed", item.price)
-        })
+        const handleRemove = (data) => items.value.splice(0,1)
 
-        return {
-            quantity,
-            increment,
-            decrement,
-            name,
-            price,
-            swapProduct,
-            total
-        }
+        return { items, handleRemove }
     }
 }
 </script>
 
 <template>
-    <h1>{{ name }} : {{ price }}</h1>
-    <h2>{{ quantity }}</h2>
-    <button @click="swapProduct">Swap</button>
-    <button @click="price++">increment price</button>
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
-
-    <h3>Total: {{ total }}</h3>
-
-
+    <CartItem v-for="item in items" :cart-item="item" :key="item.id" @remove="handleRemove"/>
 </template>
 
 <style scoped></style>
---
+++ composition-api/src/components/CartItem.vue
@@ -0,0 +1,49 @@
+<script>
+import { computed, reactive, toRefs } from "vue"
+export default {
+    props: {
+      cartItem: {
+          type: Object,
+          required: true
+      }
+    },
+    emits: ["remove"],
+    setup(props, { emit }) {
+        const item = reactive(props.cartItem)
+
+        const increment = () => item.quantity++
+        const decrement = () => item.quantity--
+
+        const { name, price, quantity } = toRefs(item)
+
+        const total = computed(() => item.price * quantity.value)
+
+        const remove = () => emit("remove", item)
+
+        return {
+            remove,
+            quantity,
+            increment,
+            decrement,
+            name,
+            price,
+            total
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ name }} : {{ price }} : {{ quantity }}</h1>
+
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <br/>
+    <button @click="remove">Remove</button>
+
+    <h3>Total: {{ total }}</h3>
+
+
+</template>
+
+<style scoped></style>

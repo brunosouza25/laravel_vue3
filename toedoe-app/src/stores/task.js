5279132a3648909d27561f2df7d068f83aa068cc
Bruno Souza <brunopw25@gmail.com>
2026-01-21 19:21:50 -0300

    Defining Getters

--- toedoe-app/src/stores/task.js
+++ toedoe-app/src/stores/task.js
@@ -1,12 +1,29 @@
-import { defineStore } from "pinia";
+import {defineStore} from "pinia";
 
 export const useTaskStore = defineStore("taskStore", {
     state: () => ({
-        tasks: [],
+        tasks: [{
+            id: 1,
+            name: "First Task",
+            is_completed: false
+        },
+            {
+                id: 2,
+                name: "Second Task",
+                is_completed: true
+            }],
         task: {
             id: 1,
             name: "First Task",
             is_completed: false
         }
-    })
+    }),
+    getters: {
+        //forma 1 de fazer o getter
+        completedTasks: (state) => state.tasks.filter(task => task.is_completed),
+        //forma 2 de fazer o getter
+        uncompletedTasks () {
+            return this.tasks.filter(task => !task.is_completed)
+        }
+    }
 })

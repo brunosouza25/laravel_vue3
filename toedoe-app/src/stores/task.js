02d10bd65fa7fd07f48cdd06ceee3147cb97110a
Bruno Souza <brunopw25@gmail.com>
2026-01-22 13:15:21 -0300

    Refactoring our Stores (Using Setup stores)

--- toedoe-app/src/stores/task.js
+++ toedoe-app/src/stores/task.js
@@ -1,27 +1,28 @@
-import {defineStore} from "pinia";
+import { ref, reactive, computed } from "vue";
+import { defineStore } from "pinia";
 import { allTasks } from "../http/task-api.js"
 
-export const useTaskStore = defineStore("taskStore", {
-    state: () => ({
-        tasks: [],
-        task: {
-            id: 1,
-            name: "First Task",
-            is_completed: false
-        }
-    }),
+const tmp = {
+    state: () => ({}),
     getters: {
-        //forma 1 de fazer o getter
-        completedTasks: (state) => state.tasks.filter(task => task.is_completed),
-        //forma 2 de fazer o getter
-        uncompletedTasks () {
-            return this.tasks.filter(task => !task.is_completed)
-        }
+
     },
     actions: {
-        async fetchAllTasks() {
-            const{ data } = await allTasks()
-            this.tasks = data.data
-        }
+
+    }
+}
+export const useTaskStore = defineStore("taskStore", () => {
+    const tasks = ref([])
+
+    const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
+    const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
+
+    const  fetchAllTasks = async() => {
+        const{ data } = await allTasks()
+        tasks.value = data.data
+    }
+
+    return {
+        tasks, completedTasks, uncompletedTasks, fetchAllTasks
     }
 })

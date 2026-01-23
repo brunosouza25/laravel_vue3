bee58200abb1b6f041c5e328c8617311a98c0fd7
Bruno Souza <brunopw25@gmail.com>
2026-01-22 22:19:09 -0300

    Navigation Guards

--- toedoe-app/src/router/index.js
+++ toedoe-app/src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router"
 import routes from "./routes.js"
 
-export default createRouter({
+const router = createRouter({
     routes,
     history: createWebHistory(),
-    linkActiveClass: "active"
+    // linkActiveClass: "active"
+});
+
+router.beforeEach((to, from) => {
+    console.log("global before each", to, from)
+    if (to.path === "/tasks"){
+        return { name: "login" }
+    }
 })
+export default router

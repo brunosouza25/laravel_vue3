3a7a82a0505f320adfbdcf16a52c61e7c315e968
Bruno Souza <brunopw25@gmail.com>
2026-01-22 22:23:20 -0300

    Route meta fields

--- toedoe-app/src/router/index.js
+++ toedoe-app/src/router/index.js
@@ -9,8 +9,13 @@ const router = createRouter({
 
 router.beforeEach((to, from) => {
     console.log("global before each", to, from)
-    if (to.path === "/tasks"){
-        return { name: "login" }
+    if (to.meta.auth){
+        return {
+            name: "login",
+            query: {
+                redirect: to.fullPath
+            }
+        }
     }
 })
 export default router
